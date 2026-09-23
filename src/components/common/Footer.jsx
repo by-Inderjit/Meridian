6eecefb1f9046942bd6963840e56a251abfcf9ba
Inderjit Singh <inderjitsingh@Inderjits-MacBook-Air.local>
2026-09-23 09:55:35 +0530

    first commit

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-full h-svh flex justify-center items-center relative z-10 bg-[#E0E0E0]">
+    FOOTER
+    </div>
+  );
+};
+
+export default Footer;
